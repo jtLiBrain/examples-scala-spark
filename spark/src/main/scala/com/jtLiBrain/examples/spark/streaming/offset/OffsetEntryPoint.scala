351eca66e00c5c299913784237039ff087704d50
jiantao.li <jtli.brain@hotmail.com>
2019-10-10 19:28:51 +0800

    try to manage the offset

--- spark/src/main/scala/com/jtLiBrain/examples/spark/streaming/offset/OffsetEntryPoint.scala
+++ spark/src/main/scala/com/jtLiBrain/examples/spark/streaming/offset/OffsetEntryPoint.scala
@@ -5,7 +5,7 @@ import org.apache.kafka.common.TopicPartition
 
 class OffsetEntryPoint {
 
-  def getLastOffsets(topic: String): Map[TopicPartition, Long] = { 
+  def getLastOffsets(topic: String): Map[TopicPartition, Long] = {
     val zkUrl: String = null
     val sessionTimeout: Int = 0
     val connectionTimeout: Int = 0
@@ -16,7 +16,7 @@ class OffsetEntryPoint {
 
     // current number of partitions from Zookeeper
     val zkNumberOfPartitionsForTopic = zkUtils.getPartitionsForTopics(Seq(topic))
-                                              .get(topic).toList.head.size
+      .get(topic).toList.head.size
 
     val persistedNumberOfPartitionsForTopic = 0
 
@@ -24,35 +24,35 @@ class OffsetEntryPoint {
 
     val fromOffsets = collection.mutable.Map[TopicPartition, Long]()
 
-    if(persistedNumberOfPartitionsForTopic == 0) {
+    if (persistedNumberOfPartitionsForTopic == 0) {
       // streaming job is started for first run
-      for (partition <- 0 to zkNumberOfPartitionsForTopic-1)
+      for (partition <- 0 to zkNumberOfPartitionsForTopic - 1)
         fromOffsets += (new TopicPartition(topic, partition) -> 0)
-    } else if(zkNumberOfPartitionsForTopic > persistedNumberOfPartitionsForTopic) {
+    } else if (zkNumberOfPartitionsForTopic > persistedNumberOfPartitionsForTopic) {
       // streaming job is restarted and the number of partitions in the topic was increased
 
       // for the original partitions
-      for (partition <- 0 to persistedNumberOfPartitionsForTopic-1) {
+      for (partition <- 0 to persistedNumberOfPartitionsForTopic - 1) {
         val fromOffset: Long = 0 // TODO
         fromOffsets += (new TopicPartition(topic, partition) -> fromOffset.toLong)
       }
 
       // for the new increased partitions
-      for (partition <- persistedNumberOfPartitionsForTopic to zkNumberOfPartitionsForTopic-1){
+      for (partition <- persistedNumberOfPartitionsForTopic to zkNumberOfPartitionsForTopic - 1) {
         fromOffsets += (new TopicPartition(topic, partition) -> 0)
       }
     } else {
       // streaming job is restarted and there are no changes to the number of partitions in the topic
-      for (partition <- 0 to persistedNumberOfPartitionsForTopic-1 ) {
+      for (partition <- 0 to persistedNumberOfPartitionsForTopic - 1) {
         val fromOffset: Long = 0 // TODO
         fromOffsets += (new TopicPartition(topic, partition) -> fromOffset.toLong)
       }
     }
 
     fromOffsets.toMap
-   }  
+  }
 
-  def commitOffsets(topic: String): Unit = {  
+  def commitOffsets(topic: String): Unit = {
 
   }
 
