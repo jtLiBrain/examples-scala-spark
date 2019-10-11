621e1e14a009278b1c63cac9a691f05a2d1d8c1a
Jason Li <man.jt.man@gmail.com>
2019-10-11 23:11:49 +0800

    add kafka module

---
+++ kafka/src/test/java/com/jtLiBrain/examples/kafka/KafkaConsumerSuite.java
@@ -0,0 +1,52 @@
+package com.jtLiBrain.examples.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.PartitionInfo;
+import org.junit.After;
+import org.junit.Before;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+import static com.jtLiBrain.examples.kafka.Utils.*;
+
+public class KafkaConsumerSuite {
+    private String topic = "test";
+
+    private KafkaConsumer<String, String> consumer;
+
+    @Before
+    public void before() {
+        Properties props = new Properties();
+
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.56.101:9092");
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "test-group5");
+        props.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.setProperty(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        props.setProperty(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+
+        consumer = new KafkaConsumer<>(props);
+
+        consumer.subscribe(Arrays.asList(topic));
+    }
+
+    @After
+    public void after() {
+        if(consumer != null)
+            consumer.close();
+    }
+
+    public void test() {
+        List<PartitionInfo> partitionsForTopic = consumer.partitionsFor(topic);
+
+        partitionsForTopic.forEach(partitionInfo -> {
+            PN(partitionInfo.toString());
+        });
+    }
+
+}
---
+++ kafka/src/test/java/com/jtLiBrain/examples/kafka/Utils.java
@@ -0,0 +1,10 @@
+package com.jtLiBrain.examples.kafka;
+
+public class Utils {
+    public static void PN(Object obj) {
+        System.out.println(obj);
+    }
+    public static void P(Object obj) {
+        System.out.print(obj);
+    }
+}
