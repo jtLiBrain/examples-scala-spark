b4317c9cdb1db118b19bcbf1fb7dd9610052fb19
Jason Li <man.jt.man@gmail.com>
2019-11-01 22:39:27 +0800

    rename to the artifactId

---
+++ scala/src/test/scala/com/jtLiBrain/examples/scala/reflection/RefExample1.scala
@@ -0,0 +1,12 @@
+package com.jtLiBrain.examples.scala.reflection
+
+import org.scalatest.FunSuite
+
+/**
+  * 1. https://docs.scala-lang.org/overviews/reflection/typetags-manifests.html
+  * 2. https://zhuanlan.zhihu.com/p/69792401
+  * 3. https://dzone.com/articles/scala-classtag-a-simple-use-case
+  */
+class RefExample1 extends FunSuite {
+
+}
