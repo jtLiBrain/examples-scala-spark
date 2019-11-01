2a19ad73362024a51e85366d4b54b6ed38113ecb
Jason Li <man.jt.man@gmail.com>
2019-11-01 23:19:20 +0800

    add scala reflection package

--- scala/src/test/scala/com/jtLiBrain/examples/scala/reflection/RefExample1.scala
+++ scala/src/test/scala/com/jtLiBrain/examples/scala/reflection/RefExample1.scala
@@ -8,5 +8,39 @@ import org.scalatest.FunSuite
   * 3. https://dzone.com/articles/scala-classtag-a-simple-use-case
   */
 class RefExample1 extends FunSuite {
+  test("via the Methods typeTag, classTag, or weakTypeTag") {
+    import scala.reflect.runtime.universe._
+    val tt = typeTag[Int]
 
+    println(tt.toString())
+
+    import scala.reflect._
+    val ct = classTag[String]
+
+    println(ct.toString())
+  }
+
+  test("Using an Implicit Parameter ") {
+    import scala.reflect.runtime.universe._
+
+    def paramInfo[T](x: T)(implicit tag: TypeTag[T]): Unit = {
+      val targs = tag.tpe match { case TypeRef(_, _, args) => args }
+      println(s"type of $x has type arguments $targs")
+    }
+
+    paramInfo(42)
+    paramInfo(List(1, 2))
+  }
+
+  test("Using a Context bound of a Type Parameter") {
+    import scala.reflect.runtime.universe._
+
+    def paramInfo[T: TypeTag](x: T): Unit = {
+      val targs = typeOf[T] match { case TypeRef(_, _, args) => args }
+      println(s"type of $x has type arguments $targs")
+    }
+
+    paramInfo(42)
+    paramInfo(List(1, 2))
+  }
 }
