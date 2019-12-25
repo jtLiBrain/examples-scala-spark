80de94d8d04e7138b94013b2b889ba2054adf9cb
Jason Li <man.jt.man@gmail.com>
2019-12-25 08:42:15 +0800

    PredictForestCover

--- spark/src/test/scala/com/jtLiBrain/examples/spark/ml/PredictForestCoverSuite.scala
+++ spark/src/test/scala/com/jtLiBrain/examples/spark/ml/PredictForestCoverSuite.scala
@@ -1,5 +1,12 @@
 package com.jtLiBrain.examples.spark.ml
 
-class PredictForestCoverSuite {
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import org.scalatest.FunSuite
 
+class PredictForestCoverSuite extends FunSuite with DataFrameSuiteBase {
+  test("d") {
+    val sparkSession = spark
+    import sparkSession.implicits._
+
+  }
 }
