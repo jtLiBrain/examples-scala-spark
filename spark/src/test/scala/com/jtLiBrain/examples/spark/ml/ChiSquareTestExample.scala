8d7ce75ff5c4e01a7183989524bd588671d91dae
Jason Li <man.jt.man@gmail.com>
2020-03-24 23:25:58 +0800

    update ml

---
+++ spark/src/test/scala/com/jtLiBrain/examples/spark/ml/ChiSquareTestExample.scala
@@ -0,0 +1,29 @@
+package com.jtLiBrain.examples.spark.ml
+
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import org.apache.spark.ml.linalg.{Vector, Vectors}
+import org.apache.spark.ml.stat.ChiSquareTest
+import org.scalatest.FunSuite
+
+class ChiSquareTestExample extends FunSuite with DataFrameSuiteBase {
+  test("test") {
+    import spark.implicits._
+
+    val data = Seq(
+      (0.0, Vectors.dense(0.5, 10.0)),
+      (0.0, Vectors.dense(1.5, 20.0)),
+      (1.0, Vectors.dense(1.5, 30.0)),
+      (0.0, Vectors.dense(3.5, 30.0)),
+      (0.0, Vectors.dense(3.5, 40.0)),
+      (1.0, Vectors.dense(3.5, 40.0))
+    )
+
+    val df = data.toDF("label", "features")
+
+    val chi = ChiSquareTest.test(df, "features", "label").head
+
+    println(s"pValues = ${chi.getAs[Vector](0)}")
+    println(s"degreesOfFreedom ${chi.getSeq[Int](1).mkString("[", ",", "]")}")
+    println(s"statistics ${chi.getAs[Vector](2)}")
+  }
+}
--- spark/src/test/scala/com/jtLiBrain/examples/spark/ml/VectorsExample.scala
+++ spark/src/test/scala/com/jtLiBrain/examples/spark/ml/VectorsExample.scala
@@ -1,7 +1,8 @@
 package com.jtLiBrain.examples.spark.ml
 
 import com.holdenkarau.spark.testing.DataFrameSuiteBase
-import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.linalg.{Vector, Vectors}
+import org.apache.spark.ml.stat.Summarizer.{mean, metrics, variance}
 import org.scalatest.FunSuite
 
 class VectorsExample extends FunSuite with DataFrameSuiteBase {
@@ -21,6 +22,35 @@ class VectorsExample extends FunSuite with DataFrameSuiteBase {
     ))
 
     println(vector.toString)
+  }
+
+  test("summarizer") {
+    import spark.implicits._
+
+    val data = Seq(
+      (Vectors.dense(2.0, 3.0, 5.0), 1.0),
+      (Vectors.dense(4.0, 6.0, 7.0), 2.0)
+    )
+
+    // [2.0, 4.0] mean:3.0    variance: 2.0 = (2.0-3.0)^2/(2-1) + (4.0-3.0)^2/(2-1)
+    // [3.0, 6.0] mean:4.5    variance: 4.5 = (3.0-4.5)^2/(2-1) + (6.0-4.5)^2/(2-1)
+    // [5.0, 7.0] mean:6.0    variance: 2.0 = (5.0-6.0)^2/(2-1) + (7.0-6.0)^2/(2-1)
+
+    val df = data.toDF("features", "weight")
+
+    val (meanVal, varianceVal) = df.select(
+      metrics("mean", "variance").summary($"features", $"weight").as("summary")
+    ).select(
+      "summary.mean", "summary.variance"
+    ).as[(Vector, Vector)].first()
+
+    println(s"with weight: mean = ${meanVal}, variance = ${varianceVal}")
+
+    val (meanVal2, varianceVal2) = df.select(
+      mean($"features"), variance($"features") // 这里是样本方差，不是总体方差
+    ).as[(Vector, Vector)].first()
+
+    println(s"without weight: mean = ${meanVal2}, variance = ${varianceVal2}")
   }
 
 }
