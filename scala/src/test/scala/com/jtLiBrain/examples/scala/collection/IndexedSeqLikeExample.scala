d09ff15a19b3be6bcc93e1505412890a130154e5
Jason Li <man.jt.man@gmail.com>
2020-05-28 22:42:38 +0800

    add IndexedSeqLikeExample and SourceExample

---
+++ scala/src/test/scala/com/jtLiBrain/examples/scala/collection/IndexedSeqLikeExample.scala
@@ -0,0 +1,7 @@
+package com.jtLiBrain.examples.scala.collection
+
+import org.scalatest.FunSuite
+
+class IndexedSeqLikeExample extends FunSuite {
+
+}
---
+++ scala/src/test/scala/com/jtLiBrain/examples/scala/io/SourceExample.scala
@@ -0,0 +1,9 @@
+package com.jtLiBrain.examples.scala.io
+
+import org.scalatest.FunSuite
+
+import scala.io.Source
+
+class SourceExample extends FunSuite {
+
+}
