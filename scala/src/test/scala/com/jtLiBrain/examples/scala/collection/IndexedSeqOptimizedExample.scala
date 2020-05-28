0bdc0a704e29ba22cfcc54a62f2f5c31429f7a0c
Jason Li <man.jt.man@gmail.com>
2020-05-28 23:18:14 +0800

    add IndexedSeqOptimizedExample

--- scala/src/test/scala/com/jtLiBrain/examples/scala/collection/IndexedSeqOptimizedExample.scala
+++ scala/src/test/scala/com/jtLiBrain/examples/scala/collection/IndexedSeqOptimizedExample.scala
@@ -3,5 +3,10 @@ package com.jtLiBrain.examples.scala.collection
 import org.scalatest.FunSuite
 
 class IndexedSeqOptimizedExample extends FunSuite {
+  test("dropWhile") {
+    val array = Array(1, 2, 3, 4, 5)
+    val changedArray = array.dropWhile(_ == 1)
 
+    assert(changedArray === Array(2, 3, 4, 5))
+  }
 }
