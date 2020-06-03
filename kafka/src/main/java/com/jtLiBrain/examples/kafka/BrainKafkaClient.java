9b3857681134f74f76f76201e3b32c8e2d4961c0
Jason Li <man.jt.man@gmail.com>
2020-06-03 23:33:00 +0800

    add BrainKafkaZkClient and BrainKafkaClient

---
+++ kafka/src/main/java/com/jtLiBrain/examples/kafka/BrainKafkaClient.java
@@ -0,0 +1,19 @@
+package com.jtLiBrain.examples.kafka;
+
+import org.apache.kafka.clients.admin.AdminClient;
+
+import java.util.Properties;
+
+public class BrainKafkaClient {
+    private AdminClient adminClient;
+
+    public void start(Properties props) {
+        adminClient = AdminClient.create(props);
+    }
+
+    public void stop() {
+        if(adminClient != null) {
+            adminClient.close();
+        }
+    }
+}
