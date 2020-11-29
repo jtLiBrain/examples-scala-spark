567e5254173ee713accebea120e10ec4e5a84ead
JT Li <jtli.brain@hotmail.com>
2020-11-29 23:08:27 +0800

    update java algorithm

--- java/src/main/java/com/jtLiBrain/examples/java/algorithm/BubbleSort.java
+++ java/src/main/java/com/jtLiBrain/examples/java/algorithm/BubbleSort.java
@@ -20,7 +20,7 @@ public class BubbleSort {
 
         int len = array.length;
 
-        for (int i = 0; i < len - 1; i++) { // N 个数字要排序完成，总共进行 N-1 趟排序
+        for (int i = 0; i < len - 1; i++) { // N 个数字要排序完成，总共进行 N-1 趟排序，每趟找出最大数，并将其往后移动
             // 第 i 趟排序的比较次数为 (N-1)-i 次，"减i"是因为每进行一趟排序都会找出一个较大值，这样上一趟找到最大值肯定不比本次的最大值小，所以无需与上一次做比较
             for (int j = 0; j < len - 1 - i; j++) {
                 if (array[j] > array[j + 1]) {
